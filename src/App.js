6386832643c96cc821976ef158c8ce6de3cecb4a
kunle niyi <58327707+adekniyi@users.noreply.github.com>
2021-05-14 12:44:39 +0100

    bugs

--- src/App.js
+++ src/App.js
@@ -2,14 +2,18 @@ import './App.css';
 import {Switch, Route} from 'react-router-dom';
 import HomePage from './HomePage'
 import ShopPage from './shop/shop.component'
+import Header from './components/header/header.component';
 
 
 function App() {
   return (
+    <>
+    <Header/>
     <Switch>
       <Route exact path="/" component={HomePage}/>
-      <Route path="/shop" component={ShopPage}/>
+      <Route exact path="/shop" component={ShopPage}/>
     </Switch>
+    </>
   );
 }
 
